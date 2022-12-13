c87f521c23680018275beca9a76b33fa9fac8ec8
刘 民 <1443619775@qq.com>
2022-12-13 15:53:23 +0800

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 import * as React from 'react'
 import {Link} from "gatsby"
 import Layout from '../components/layouts'
+import {StaticImage} from 'gatsby-plugin-image'
 const IndexPage = ()=>
 {
     return (
         <Layout pageTitle="Home">
             <p>Something</p>
+            <StaticImage
+                alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+                src="https://img1.baidu.com/it/u=1526096303,4228783601&fm=253&fmt=auto&app=138&f=JPEG?w=749&h=500"
+            />
         </Layout>
     )
 }
