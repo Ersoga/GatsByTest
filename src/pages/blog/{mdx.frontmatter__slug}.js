554bece763631ee15ad9ffdaa941cb51a2c17dc5
刘 民 <1443619775@qq.com>
2022-12-15 16:15:36 +0800

    Finished Gatsby Tutorial Part 7

--- src/pages/blog/{mdx.frontmatter__slug}.js
+++ src/pages/blog/{mdx.frontmatter__slug}.js
@@ -2,9 +2,15 @@ import * as React from 'react'
 import Layout from '../../components/layouts'
 import Seo from '../../components/seo'
 import { graphql } from 'gatsby'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 const BlogPost = ({data,children})=>{
+  const image = getImage(data.mdx.frontmatter.hero_image)
     return (
         <Layout pageTitle={data.mdx.frontmatter.name}>
+            <GatsbyImage image={image} alt={data.mdx.frontmatter.hero_image_alt}></GatsbyImage>
+            <p>Photo Credit:{" "}
+              <a href={data.mdx.frontmatter.hero_image_credit_link}>{data.mdx.frontmatter.hero_image_credit_text}</a>
+            </p>
             <p>{data.mdx.frontmatter.datePublished}</p>
             {children}
         </Layout>
@@ -18,6 +24,14 @@ query ($id: String) {
     frontmatter {
       name
       datePublished(formatString: "MMMM D, YYYY")
+      hero_image_alt
+      hero_image_credit_link
+      hero_image_credit_text
+      hero_image{
+        childImageSharp{
+          gatsbyImageData
+        }
+      }
     }
   }
 }
